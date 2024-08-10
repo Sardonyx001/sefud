402ca26bfd5ee1fb6fa2edd35e7ebd66ca30f62a
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2024-08-10 10:23:20 +0900

    feat: ♻️ add strctured and pretty logging via log/slog & charmbracelet/log

--- server/server.go
+++ server/server.go
@@ -1,7 +1,13 @@
 package server
 
 import (
+	"context"
+	"log/slog"
+	"os"
+	"time"
+
 	"github.com/Sardonyx001/sefud/handlers"
+	"github.com/charmbracelet/log"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -13,7 +19,40 @@ type Server struct {
 func New() *Server {
 	e := echo.New()
 
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
+	logHandler := log.NewWithOptions(os.Stderr, log.Options{
+		ReportCaller:    true,
+		ReportTimestamp: true,
+		TimeFormat:      time.RFC3339,
+	})
+	logger := slog.New(logHandler)
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		LogMethod:   true,
+		LogLatency:  true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
+					slog.String("method", v.Method),
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("latency", v.Latency.String()),
+				)
+			} else {
+				logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
+					slog.String("method", v.Method),
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("latency", v.Latency.String()),
+					slog.String("err", v.Error.Error()),
+				)
+			}
+			return nil
+		},
+	}))
 	e.Use(middleware.Recover())
 
 	// Routes
