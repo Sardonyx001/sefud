640ae7f70eb458b5ee577ac26eeddd1ae2be2b7a
jam <lassouedjamel99@gmail.com>
2025-08-04 17:51:46 +0900

    Revert "feat: implement Swagger UI for API documentation"
    This reverts commit 23f32627aaebc03c72b8e3b15816bc8feaad3fb1.

--- server/server.go
+++ server/server.go
@@ -8,7 +8,6 @@ import (
 	"github.com/Sardonyx001/sefud/logger"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	echoSwagger "github.com/swaggo/echo-swagger"
 	"gorm.io/gorm"
 )
 
@@ -24,13 +23,10 @@ func New(cfg *config.Config) *Server {
 	e.Use(logger.InitLoggerMiddleware())
 	e.Use(middleware.Recover())
 
-	// API Routes
+	// Routes
 	e.POST("/up", handlers.UploadFile)
 	e.GET("/:id", handlers.DownloadFile)
 	e.DELETE("/:id", handlers.DeleteFile)
-
-	// Swagger documentation route
-	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	return &Server{
 		echo:   e,
